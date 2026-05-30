022b9f02ebf1ff53aa81f325deebfa51eef82967
Kayamuddin-Khan05 <khankayamuddin634@gmail.com>
2026-05-30 12:00:32 +0530

    Cart state maintain on refresh with localStorage

--- src/app/cartSlice.js
+++ src/app/cartSlice.js
@@ -1,22 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    count: 0,
-    products: []
+    count: localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")).length : 0,
+    products: localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
 }
 
-const addItemAction = (state, action) => {
+const addItemReducer = (state, action) => {
     const product = state.products.find((p) => p.id === action.payload.id);
     if (product) {
         product.quantity += 1;
-        return;
     } else {
         state.products.push({ ...action.payload, quantity: 1 });
+        state.count = state.products.length;
     }
-    state.count = state.products.length;
+    localStorage.setItem("cart", JSON.stringify(state.products));
 }
 
-const checkoutAction = (state) => {
+const checkoutReducer = (state) => {
     let totalPrice = 0;
     state.products.forEach((product) => {
         totalPrice += product.price * product.quantity;
@@ -24,19 +24,49 @@ const checkoutAction = (state) => {
     alert(`Total Price: $${Math.round(totalPrice)}`);
     state.count = 0;
     state.products = [];
+    localStorage.removeItem("cart");
 }
 
-const addToCart = createSlice({
+const increaseQuantityReducer = (state, action) => {
+    const product = state.products.find((p) => p.id === action.payload);
+    if (product) {
+        product.quantity += 1;
+        localStorage.setItem("cart", JSON.stringify(state.products));
+    }
+}
+
+const decreaseQuantityReducer = (state, action) => {
+    const product = state.products.find((p) => p.id === action.payload);
+    if (product) {
+        product.quantity -= 1;
+        if (product.quantity < 1) {
+            state.products = state.products.filter((p) => p.id !== action.payload);
+        }
+        localStorage.setItem("cart", JSON.stringify(state.products));
+    }
+}
+
+const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
-        addItem: addItemAction,
-        checkout: checkoutAction
+        addItem: addItemReducer,
+        checkout: checkoutReducer,
+        removeFromCart: (state, action) => {
+            state.products = state.products.filter((product) => product.id !== action.payload);
+            state.count = state.products.length;
+            localStorage.setItem("cart", JSON.stringify(state.products));
+        },
+        increaseQuantity: increaseQuantityReducer,
+        decreaseQuantity: decreaseQuantityReducer
     }
 })
 
 export const {
     addItem,
-    checkout
-} = addToCart.actions;
-export default addToCart.reducer;
+    checkout,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity
+} = cartSlice.actions;
+export default cartSlice.reducer;
--- src/features/home/Header.jsx
+++ src/features/home/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, Trash2Icon, SquarePlusIcon, SquareMinusIcon } from "lucide-react";
 import { useSelector, useDispatch } from "react-redux";
-import { checkout } from "../../app/cartSlice.js";
+import { checkout, removeFromCart, increaseQuantity, decreaseQuantity } from "../../app/cartSlice.js";
 
 
 function Home() {
@@ -63,7 +63,7 @@ function Home() {
 
                     {/* CART BOX */}
                     {openCart && (
-                        <div className="absolute top-14 right-0 w-[320px] bg-white rounded-xl shadow-xl p-4 z-50">
+                        <div className="absolute top-14 right-0 w-[320px] bg-white rounded-xl shadow-xl p-4 z-50 select-none">
 
                             <h2 className="text-xl font-bold mb-4">
                                 Cart Items
@@ -75,39 +75,57 @@ function Home() {
                                     Your cart is empty.
                                 </div>
                             ) : (
-                                products.map((product) => {
-                                    return (
-                                        <div key={product.id} className="flex items-center gap-3 border-b pb-3 mb-3">
+                                <div className="text-gray-500 max-h-80 overflow-y-auto scrollbar-none">
+                                    {products.map((product) => {
+                                        return (
+                                            <div key={product.id} className="flex items-center gap-3 border-b pb-3 mb-3">
 
-                                            <img
-                                                src={product.images?.[0] || ""}
-                                                alt=""
-                                                className="w-16 h-16 rounded-lg object-cover"
-                                            />
+                                                <img
+                                                    src={product.images?.[0] || ""}
+                                                    alt=""
+                                                    className="w-16 h-16 rounded-lg object-cover"
+                                                />
 
-                                            <div className="flex-1">
-                                                <h3 className="font-semibold">
-                                                    {product.title}
-                                                </h3>
+                                                <div className="flex-1">
+                                                    <h3 className="font-semibold">
+                                                        {product.title}
+                                                    </h3>
 
-                                                <p className="text-gray-500 text-sm">
-                                                    ${product.price}
-                                                </p>
+                                                    <p className="text-gray-500 text-sm">
+                                                        ${product.price}
+                                                    </p>
+                                                </div>
+                                                <div className="flex flex-col items-center justify-center">
+                                                    <span className="text-gray-500 select-none">Quantity:</span>
+                                                    <div className="flex items-center justify-center gap-2 mt-1 n">
+                                                        <SquarePlusIcon className="w-5 h-5 text-gray-500 hover:text-green-500 cursor-pointer"
+                                                            onClick={() => dispatch(increaseQuantity(product.id))}
+                                                        />
+                                                        <span className="text-gray-500 select-none">{product.quantity}</span>
+                                                        {product.quantity > 1 ? (
+                                                            <SquareMinusIcon className="w-5 h-5 text-gray-500 hover:text-yellow-500 cursor-pointer"
+                                                                onClick={() => dispatch(decreaseQuantity(product.id))}
+                                                            />
+                                                        ) : (
+                                                            <Trash2Icon className="w-5 h-5 text-gray-500 hover:text-red-500 cursor-pointer"
+                                                                onClick={() => dispatch(removeFromCart(product.id))}
+                                                            />)}
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div >
-                                                quantity: <span className="text-gray-500">{product.quantity}</span>
-                                            </div>
-                                        </div>
-                                    )
-                                })
+                                        )
+                                    })}
+                                </div>
                             )}
 
                             {/* BUTTON */}
-                            <button className="w-full bg-blue-500 hover:bg-blue-600 duration-300 text-white py-3 rounded-lg font-medium"
-                                onClick={() => dispatch(checkout())}
-                            >
-                                Checkout
-                            </button>
+                            <div className="w-full">
+                                <button className="w-full bg-blue-500 hover:bg-blue-600 duration-300 text-white py-3 rounded-lg font-medium"
+                                    onClick={() => dispatch(checkout())}
+                                >
+                                    Checkout
+                                </button>
+                            </div>
 
                         </div>
                     )}
